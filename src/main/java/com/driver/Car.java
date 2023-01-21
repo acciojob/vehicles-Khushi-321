d0906e6f5702683e962826e98a3265ac4e25b2be
unknown <khushietw2002@gmail.com>
2023-01-21 20:14:10 +0530

    Vehicles

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -5,20 +5,43 @@ public class Car extends Vehicle {
     private String type;
     private int doors;
     private int gears;
+
+    public boolean isManual() {
+        return isManual;
+    }
+
+    public void setManual(boolean manual) {
+        isManual = manual;
+    }
+
+    public void setGears(int gears) {
+        this.gears = gears;
+    }
+
     private boolean isManual;
     private int currentGear;
     private int seats;
 
-    public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats) {
+    public Car(String name, int wheels, int doors,
+               int gears, boolean isManual, String type, int seats) {
         //Hint: Car extends Vehicle
+        super(name);
+        this.wheels=wheels;
+        this.type=type;
+        this.gears=gears;
+        this.isManual=isManual;
+        this.currentGear=1;
+        this.seats=seats;
     }
 
     public void changeGear(int newGear){
+        this.currentGear=newGear;
 
         System.out.println("changeGear method called - The gear is changed to: " + currentGear);
     }
 
     public void changeSpeed(int newSpeed, int newDirection){
+        move(newSpeed,newDirection);
 
         System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
     }
--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -4,10 +4,12 @@ public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
         //Use arbitrary values for parameters which are not mentioned
+        super(name,4,2,7,isManual,"Sport",2);
+
     }
 
     public void accelerate(int rate){
-        int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
+        int newSpeed = rate+getCurrentSpeed(); //set the value of new speed by using currentSpeed and rate
         /**
          * speed 0: gear 1
          * speed 1-50: gear 1
@@ -20,6 +22,8 @@ public class F1 extends Car {
 
         if(newSpeed == 0) {
             //Stop the car, set gear as 1
+            stop();
+            setGears(1);
         }
         //for all other cases, change the gear accordingly
 
